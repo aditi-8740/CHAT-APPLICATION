af299ff22a056e37c08505d399c2af8bdae9cba7
aditi-8740 <148616437+aditi-8740@users.noreply.github.com>
2024-11-07 16:30:17 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import Register from "./Pages/Register"
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:8000'
+  axios.defaults.baseURL = API_URL
   axios.defaults.withCredentials = true
   return (
     <>
